4355c807f3e4c17a349982de28a4d358a858db1c
Nguyễn Công Dũng <dung.dev.gramer@gmail.com>
2020-11-29 01:12:14 +0700

    Fix delay effect when click

--- script.js
+++ script.js
@@ -7,33 +7,35 @@ fetch("./keyData.json")
 		res.map((x) => {
 			drumpad.innerHTML += `<li class="key" data-key="${x.key}"><audio src="assets/media/${x.url}" data-key="${x.key}"></audio></li>`;
 		})
-	);
+	).then(() => {
+		function removeTransition(e) {
+			console.log(e);
+			if (e.propertyName !== "background-color") return;
+			this.classList.remove("playing");
+		}
+		
+		function playSound(e) {
+			const audio = document.querySelector(`audio[data-key=${e.code}]`);
+			const key = document.querySelector(`.key[data-key=${e.code}]`);
+			if (!audio) return;
+			audio.currentTime = 0;
+			audio.play();
+			key.classList.add("playing");
+		}
+		
+		const keys = document.querySelectorAll(".key");
+		keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+		
+		window.addEventListener("keydown", playSound);
+		
+		window.addEventListener("click", function (e) {
+			const audio = document.querySelector(
+				`audio[data-key=${e.path[0].getAttribute("data-key")}]`
+			);
+			if (!audio) return;
+			audio.currentTime = 0;
+			audio.play();
+		});
+	});
 
-function removeTransition(e) {
-	console.log(e);
-	if (e.propertyName !== "background-color") return;
-	this.classList.remove("playing");
-}
 
-function playSound(e) {
-	const audio = document.querySelector(`audio[data-key=${e.code}]`);
-	const key = document.querySelector(`.key[data-key=${e.code}]`);
-	if (!audio) return;
-	audio.currentTime = 0;
-	audio.play();
-	key.classList.add("playing");
-}
-
-const keys = document.querySelectorAll(".key");
-keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
-
-window.addEventListener("keydown", playSound);
-
-window.addEventListener("click", function (e) {
-	const audio = document.querySelector(
-		`audio[data-key=${e.path[0].getAttribute("data-key")}]`
-	);
-	if (!audio) return;
-	audio.currentTime = 0;
-	audio.play();
-});
